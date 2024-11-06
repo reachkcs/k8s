6f4bdf14cdda6363eaee4353968ca2e937d54bd0
Sreedhar Chidambaram <reach_sreedhar@yahoo.com>
2024-11-06 05:20:43 -0500

    Enhancements

--- nginx/cleanup.bash
+++ nginx/cleanup.bash
@@ -1,4 +1,13 @@
 #!/bin/bash -x
+type -i NS_FOUND=$(kubectl get namespaces | grep ^nginx | wc -l)
+
+if [ ${NS_FOUND} -eq 0 ];then
+  echo "Namespace nginx is not there. Creating it."
+  kubectl create namespace nginx
+else
+  echo "Namespace nginx is found.
+fi
+
 kubectl delete -f nginx-deployment.yaml
-sleep 10
+#sleep 10
 kubectl delete -f nginx-ingress.yaml
--- nginx/rebuild.bash
+++ nginx/rebuild.bash
@@ -1,4 +1,18 @@
-#!/bin/bash -x
+#!/bin/bash
+
+declare -i NS_FOUND
+NS_FOUND=$(kubectl get namespaces | grep ^nginx | wc -l)
+
+echo "<${NS_FOUND}>"
+
+if [ ${NS_FOUND} -eq 0 ];then
+  echo "Namespace nginx is not there. Creating it."
+  kubectl create namespace nginx
+else
+  echo "Namespace nginx is found."
+fi
+
+set -x
 kubectl apply -f nginx-deployment.yaml
-sleep 10
+#sleep 10
 kubectl apply -f nginx-ingress.yaml
